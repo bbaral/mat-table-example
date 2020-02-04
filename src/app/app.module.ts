393a29acfd02f16c7914387f5c1f669df320770b
bbaral <baralbikram@hotmail.com>
2020-02-03 23:12:58 -0500

    mat-table example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material';
+import { TableListComponent } from './table-list/table-list.component';
+import { DisplayTableComponent } from './display-table/display-table.component';
+import { AddRowComponent } from './add-row/add-row.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableListComponent,
+    DisplayTableComponent,
+    AddRowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
